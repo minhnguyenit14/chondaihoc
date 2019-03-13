3384bacb86102e510fd438fda8a69971a3ff2bed
Minh Nguyen <minhnguyenit14@gmail.com>
2019-03-13 15:11:13 +0700

    5

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/build/config.json'));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
- return res.send('pong');
+  return res.send('pong');
 });
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
